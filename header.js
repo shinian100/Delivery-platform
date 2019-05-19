974ce2a88f939ba164e2d9abe3c4d7db0facc9ce
shinian100 <49868895+shinian100@users.noreply.github.com>
2019-05-19 10:44:46 +0800

    Add files via upload

--- header.js
+++ header.js
@@ -1,26 +1,10 @@
-(function(){
- var img=document.querySelector("div.container-fluid>div.row>div>ul>li>a>img");
- var d1=document.getElementById("d1");
- var d2=document.getElementById("d2");
- img.onclick=function(){
-    // var img=this;
-    if(img.value==="../images/submit.png"){
-        d2.style.display="none";
-        // d1.style.display="block";
-    }else{
-        // d1.style.display="none";
-        d2.style.display="block";
-    }
-};
- var img_one=document.querySelector("div.container-fluid>div.fixed-top>form>a>img");
- img_one.onclick =function(){
-  // var img_one=this;
-  if(img_one.value==="../images/exits.png"){
-      // d1.style.display="none";
-      d2.style.display="block";
-  }else{
-      d2.style.display="none";
-      // d1.style.display="block";
-  }
- };
-})();
+$(function(){
+    $.ajax({
+       url:"header.html",
+       type:"get",
+       success:function(html){
+          $(html).replaceAll("#header");
+          $(`<link rel="stylesheet" href="../css/header.css">`).appendTo("head");
+       }
+    })
+ });
